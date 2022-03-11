01344f933c6388adb8f0de82da971ec0b1d97166
Adir <adirmelker1@gmail.com>
2022-03-11 16:10:29 +0200

    adir1

--- anlyze_work1.py
+++ anlyze_work1.py
@@ -1,5 +1,7 @@
 """print(abs(3.0*(4.0/3.0-1)-1)) - this is the code before the correction"""
 epsilon=1.0
-while (1.0+0.5*epsilon)>1
-    epsilon=0.5
+while (1.0+0.5*epsilon)>1:
+    epsilon=0.5*epsilon
 
+answer=abs((3.0*(4.0/3.0-1)-1)+epsilon)
+print(answer)
