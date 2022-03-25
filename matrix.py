447ed53bfef066158e0ee47f83f5b91e579a9233
Guy Shabtay <guysh16@ac.sce.ac.il>
2022-03-25 12:08:07 +0300

    update 0f the result and b

--- matrix.py
+++ matrix.py
@@ -32,34 +32,59 @@ def det(matrix):
     else:
         print(f"the determinant is: {determinant}")
 
-def elementaryMatrix(matrix):
+
+def elementaryMatrix(matrix,b):
     size=len(matrix)
     mulOfAllElememtary = identityMatrix(size)
+    print(f"matrix: ")
+    printMatrix(matrix)
+
+
+    print(f"sorted: ")
+    printMatrix(matrix)
     e=1
     for i in range(size):
         elementary = identityMatrix(size)
         if matrix[i][i]!=1:
             elementary[i][i]=1/matrix[i][i]
+            print(f"matrix: ")
+            printMatrix(matrix)
+
+
+
+            print(f"sorted: ")
+            printMatrix(matrix)
             print(f"E{e}:")
             e=e+1
             printMatrix(elementary)
-            matrix = multi(elementary, matrix)
+            print(f"multi: ")
+            matrix=multi(elementary, matrix)
             printMatrix(matrix)
             mulOfAllElememtary = multi(elementary, mulOfAllElememtary)
+
 
         j = 0
         for j in range(size):
             if matrix[j][i]!=0 and j!=i :
                 elementary = identityMatrix(size)
                 elementary[j][i]=matrix[j][i]*-1 #למה צריך לחלק באיבר המוביל? הוא תמיד 1
+                print(f"matrix: ")
+                printMatrix(matrix)
+
+
+
+                print(f"sorted: ")
+                printMatrix(matrix)
                 print(f"E{e}:")
-                e=e+1
+                e = e + 1
                 printMatrix(elementary)
-                matrix=multi(elementary, matrix)
+                print(f"multi: ")
+                matrix = multi(elementary, matrix)
                 printMatrix(matrix)
                 mulOfAllElememtary = multi(elementary, mulOfAllElememtary)
     print("the The inverse matrix is: ")
     printMatrix(mulOfAllElememtary)
+    print(b)
 
 
 def multi(m1,m2):
@@ -94,39 +119,68 @@ def sort(matrix):
             pivot,max=col,col
             for j in range(size-col-1):
                 row+=1
-                if abs(matrix[row][col])>abs(matrix[pivot][pivot]):
+                if abs(matrix[row][col])>abs(matrix[pivot][pivot]) and abs(matrix[row][col])>abs(matrix[max][col]):
                     max=row
             if max!=pivot:
                 temp=matrix[pivot]
                 matrix[pivot]=matrix[max]
                 matrix[max]=temp
-    print("sort:")
-    printMatrix(matrix)
     return matrix
 
+def sortB(matrix,b):
+    size=len(matrix)
+    for col in range(size):
+            row=col
+            pivot,max=col,col
+            for j in range(size-col-1):
+                row+=1
+                if abs(matrix[row][col])>abs(matrix[pivot][pivot]) and abs(matrix[row][col])>abs(matrix[max][col]):
+                    max=row
+            if max!=pivot:
+                temp=b[pivot]
+                temp2 = matrix[pivot]
+                b[pivot]=b[max]
+                matrix[pivot] = matrix[max]
+                b[max]=temp
+                matrix[max] = temp2
+    return b
 
 
 mat1 = [[2,-3, -4],
         [0, 0, -1],
         [1, -2, 1]]
 
-b = [[16], [-8],[0]]
+
 
 mat2 = [[1, -1, -2],
         [2, -3, -5],
         [-1, 3, 5]]
+b2 = [-7, -19,20]
 
-mat3 = [[1, 0, 2],
-        [2, -1, 3],
-        [4, 1, 8]]
+mat3 = [[2, 0, -1],
+        [5, 1, 0],
+        [0, 1, 3]]
+b3 = [-1, 7,11]
 
-mat4 = [[1,0,2],
-        [2,-1,3],
-        [4,1,8]]
 
-det(mat3)
+mat4 = [[3,-2,4],
+        [1,0,2],
+        [0,1,0]]
+b4 = [11, 7,2]
 
-elementaryMatrix(sort(mat3))
+"""det(mat2)
+b2=sortB(mat2,b2) # ממיין פעם אחת את איי ופעם אחת את בי בגלל שהם באותו סקואופ
+#mat2=sort(mat2)
+elementaryMatrix(mat2,b2)"""
+
+"""det(mat3)
+b3=sortB(mat3,b3) # ממיין פעם אחת את איי ופעם אחת את בי בגלל שהם באותו סקואופ
+elementaryMatrix(mat3,b3)"""
+
+det(mat4)
+b4=sortB(mat4,b4) # ממיין פעם אחת את איי ופעם אחת את בי בגלל שהם באותו סקואופ
+#mat2=sort(mat2)
+elementaryMatrix(mat4,b4)
 
 
 #multi([[1, 0, 0], [-2, 1, 0], [0, 0, 1]],[[1, -1, -2], [2, -3, -5], [-1, 3, 5]],3)
